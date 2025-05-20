cd4b7ddce0689ee28110babc71b2a9d75756c2f1
Harsha-S2604 <harsha91148@outlook.com>
2025-05-20 16:08:54 +0530

    feat: syntax check for the json string

--- ujson.c
+++ ujson.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 #include "ujson.h"
 
-void ujson_parser(char *s) {
-    printf("parsing...");
+int braces_are_valid(char* json_string, int total_characters) {
+    char opening_braces = json_string[0];
+    char closing_braces = json_string[total_characters - 1];
+    int valid_braces = opening_braces == '{' && closing_braces == '}';
+    return valid_braces;
+}
+
+JSON ujson_parser(char* json_string) {
+    JSON json_value = {};
+
+    int total_characters = strlen(json_string);
+
+    int valid_braces = braces_are_valid(json_string, total_characters);
+    if (!valid_braces) {
+        printf("[PARSING FAILED]:: invalid syntax\n");
+    } else {
+        printf("Parse the json\n");
+    }
+    return json_value;
 }
--- ujson.h
+++ ujson.h
@@ -1,4 +1,20 @@
 #ifndef UJSON_H
 #define UJSON_H
-void ujson_parser(char* s);
+
+enum DataTypes {
+    STRING,
+    INTEGER,
+    FLOAT,
+    ARRAY_OF_STRINGS,
+    ARRAY_OF_INTEGERS,
+    ARRAY_OF_FLOATS,
+};
+
+typedef struct {
+    enum DataTypes dataType;
+    char* key;
+    char* strValue;
+} JSON;
+
+JSON ujson_parser(char* s);
 #endif
